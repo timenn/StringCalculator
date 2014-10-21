3cc8af0b6599a1f0ba8321ef74de4c47b2913236
Timen van den Berg <timen.vandenberg@ogd.nl>
2014-10-21 12:03:37 +0200

    Allow an unknown amount of numbers as input

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -41,4 +41,14 @@ class StringCalculatorTest extends PHPUnit_Framework_TestCase
         $this->assertSame(5, $calculator->add("2,3"));
     }
 
+    /**
+     * @test
+     **/
+    public function ManyNumbersAsInputReturnsTheSumOfThoseNumbers()
+    {
+        $calculator = $this->getCalculator();
+
+        $this->assertSame(55, $calculator->add("0,1,2,3,4,5,6,7,8,9,10"));
+    }
+
 }
